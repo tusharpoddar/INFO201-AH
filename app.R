a282f5cbd40663ec51bb9470053a4a52f8de8b6f
hebertn <hebertn@uw.edu>
2019-11-15 23:31:59 -0800

    rev1 to app.R

--- app.R
+++ app.R
@@ -10,10 +10,30 @@ intro_page <- tabPanel(
     )
 
 )
-
+br <- tags$br()
 about_us <- tabPanel(
     "About Us",
-    
+    fluidPage(
+        h1("Allison Bennet, Nick Hebert, Tushar Poddar, Mariella Gauvreau"),
+        br,
+        h2("Allison"),
+        p("photo"),
+        p("description"),
+        br,
+        h2("Nick"),
+        p("photo"),
+        p("description"),
+        br,
+        h2("Tushar"),
+        p("photo"),
+        p("description"),
+        br,
+        h2("Mariella"),
+        p("photo"),
+        p("description"),
+        br
+        
+    )
 )
 
 about_the_tech <- tabPanel(
@@ -30,20 +50,44 @@ findings <- tabPanel(
     "Findings",
     
 )
+neighborhood <- tabPanel(
+    "Select by Neighborhood",
+    value = 'neighborhood'
+)
+crime_rates <- tabPanel(
+    "Select by Crime Rates",
+    value = 'crime_rates'
+)
+ratings <- tabPanel(
+    "Select by Rating",
+    value = 'ratings'
+)
 
 # Define UI ----
 ui <- navbarPage(
     "Crime and Airbinb",
+    id = "toptabs",
     intro_page,
     about_us,
     about_the_tech,
     background,
-    findings
-    
+    findings,
+    neighborhood,
+    crime_rates,
+    ratings
 )
 
 # Define server logic ----
-server <- function(input, output) {
+server <- function(input, output, session) {
+    observeEvent(eventExpr = input$nselect, {
+        updateTabsetPanel(session, 'toptabs', selected = 'neighborhood')
+    })
+    observeEvent(eventExpr = input$cselect, {
+        updateTabsetPanel(session, 'toptabs', selected = 'crime_rates')
+    })
+    observeEvent(eventExpr = input$rselect, {
+        updateTabsetPanel(session, 'toptabs', selected = 'ratings')
+    })
 }
 
 shinyApp(ui, server)
